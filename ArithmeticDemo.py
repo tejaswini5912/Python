5fcc0c16711de671a44a164df596996fad955523
tejaswini5912 <161304059+tejaswini5912@users.noreply.github.com>
2024-03-12 19:53:35 +0530

    Add files via upload

---
+++ ArithmeticDemo.py
@@ -0,0 +1,34 @@
+a=57
+b=10
+print(2**3)
+print(a+b)
+print(a//b)
+p=10
+q=10
+print(p==q)
+print(p is q)
+x=2220
+y=10
+print(x is y)
+c=10
+print(a is b)
+c=a
+print(a is c)
+print(id(a))
+print(id(c))
+x="cmrec"
+y="hello"
+z="cmrec"
+print(x==z)
+print(x is z)
+k=x
+print(id(x))
+print(k is x)
+m=["cseec","ec"]
+n=["cseec","ec"]
+o=m
+print(o is m)
+print(m is n)
+v=[1,2,3,10,20,30]
+print(5 in v)
+print(5 not in v)
---
+++ compoundinterest.py
@@ -0,0 +1,5 @@
+principal=int(input("Enter the principal Amount"))
+rate=float(input("rate of interest"))
+time=int(input("time period"))
+ci=(principal*rate*time)/100
+print("Compound interest=",ci)
